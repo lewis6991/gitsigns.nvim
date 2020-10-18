a993f848e0aba47e9f2875ea3cbab893cce3d16a
Lewis Russell <lewis6991@gmail.com>
2020-10-18 11:19:06 +0100

    Init

---
+++ lua/gitsigns/async.lua
@@ -0,0 +1,86 @@
+-- Originated from: https://github.com/ms-jpq/neovim-async-tutorial/blob/neo/lua/async.lua
+
+local co = coroutine
+
+
+-- use with wrap
+local pong = function (func, callback)
+  assert(type(func) == "function", "type error :: expected func")
+  local thread = co.create(func)
+  local step = nil
+  step = function (...)
+    local stat, ret = co.resume(thread, ...)
+    assert(stat, ret)
+    if co.status(thread) == "dead" then
+      (callback or function () end)(ret)
+    else
+      assert(type(ret) == "function", "type error :: expected func")
+      ret(step)
+    end
+  end
+  step()
+end
+
+
+local awrap = function (func)
+  assert(type(func) == "function", "type error :: expected func")
+  return function(...)
+    local params = {...}
+    return function(step)
+      table.insert(params, step)
+      return func(unpack(params))
+    end
+  end
+end
+
+
+-- many thunks -> single thunk
+local join = function (thunks)
+  return function (step)
+    if #thunks == 0 then
+      return step()
+    end
+    local to_go = #thunks
+    local results = {}
+    for i, thunk in ipairs(thunks) do
+      assert(type(thunk) == "function", "thunk must be function")
+      local callback = function (...)
+        results[i] = {...}
+        if to_go == 1 then
+          step(unpack(results))
+        else
+          to_go = to_go - 1
+        end
+      end
+      thunk(callback)
+    end
+  end
+end
+
+
+-- sugar over coroutine
+-- @param defer : Function to defer
+-- @param ...   : Arguments to pass to defer
+local await = function(defer, ...)
+  assert(type(defer) == "function", "type error :: expected func")
+  return co.yield(awrap(defer)(...))
+end
+
+
+local await_all = function(defer)
+  assert(type(defer) == "table", "type error :: expected table")
+  return co.yield(join(defer))
+end
+
+-- Goto main event loop
+local await_main = function()
+  return await(vim.schedule)
+end
+
+return {
+  async      = awrap(pong),
+  await      = await,
+  await_all  = await_all,
+  awrap      = awrap,
+  await_main = await_main
+}
