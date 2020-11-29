37861062880f25ec01750e4875631de85c05a457
Lewis Russell <lewis.russell@arm.com>
2020-11-29 17:14:05 +0000

    Fix #24

--- lua/gitsigns/debug.lua
+++ lua/gitsigns/debug.lua
@@ -5,13 +5,7 @@ local M = {}
 
 function M.dprint(msg, bufnr, caller)
   local name = caller or debug.getinfo(2, 'n').name or ''
-  if not dfile then
-    print('Opening '..log)
-    dfile = io.open(log, 'w')
-    dfile:write('\n--------------------NEW SESSION--------------------\n\n')
-  end
-  dfile:write(string.format('%s(%s): %s\n', name, bufnr, msg))
-  dfile:flush()
+  print(string.format('%s(%s): %s\n', name, bufnr, msg))
 end
 
 return M
