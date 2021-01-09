977b26f213588b6d3e0ac4c819fff79e8a7a6eaf
Lewis Russell <lewis6991@gmail.com>
2021-01-09 11:14:30 +0000

    Handle when files are removed from the index
    For scenarios when the branch is changed to one that doesn't contain a
    file that we are attached to.
    
    Fixes #44

--- lua/gitsigns/status.lua
+++ lua/gitsigns/status.lua
@@ -1,23 +1,24 @@
 local api = vim.api
 
 local Status = {
-  laststatus = {},
+  status = {},
   formatter = nil
 }
 
-function Status:update_vars(bufnr, status)
+function Status:update(bufnr, status)
+  if status then
+    self.status = status
+  end
   vim.schedule(function()
-    api.nvim_buf_set_var(bufnr, 'gitsigns_head', status.head or '')
-    api.nvim_buf_set_var(bufnr, 'gitsigns_status_dict', status)
-    api.nvim_buf_set_var(bufnr, 'gitsigns_status', self.formatter(status))
+    api.nvim_buf_set_var(bufnr, 'gitsigns_head', self.status.head or '')
+    api.nvim_buf_set_var(bufnr, 'gitsigns_status_dict', self.status)
+    api.nvim_buf_set_var(bufnr, 'gitsigns_status', self.formatter(self.status))
   end)
-  self.laststatus = status
 end
 
-function Status:update_head_var(bufnr, head)
-  local status = self.laststatus
-  status.head = head
-  Status:update_vars(bufnr, status)
+function Status:update_head(bufnr, head)
+  self.status.head = head
+  Status:update(bufnr)
 end
 
 return Status
