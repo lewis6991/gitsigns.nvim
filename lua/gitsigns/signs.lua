a5f3484b6c19a22160f956be2f046ad3d6ae2fb4
Lewis Russell <lewis6991@gmail.com>
2021-01-17 14:13:07 +0000

    Port lua to teal
    Create README.md for lua files

--- lua/gitsigns/signs.lua
+++ lua/gitsigns/signs.lua
@@ -4,19 +4,19 @@ local M = {}
 local sign_define_cache = {}
 
 local function sign_get(name)
-  if not sign_define_cache[name] then
-    local s = vim.fn.sign_getdefined(name)
-    if not vim.tbl_isempty(s) then
-      sign_define_cache[name] = s
-    end
-  end
-  return sign_define_cache[name]
+   if not sign_define_cache[name] then
+      local s = vim.fn.sign_getdefined(name)
+      if not vim.tbl_isempty(s) then
+         sign_define_cache[name] = s
+      end
+   end
+   return sign_define_cache[name]
 end
 
 function M.sign_define(name, opts)
-  if not sign_get(name) then
-    vim.fn.sign_define(name, opts)
-  end
+   if not sign_get(name) then
+      vim.fn.sign_define(name, opts)
+   end
 end
 
 return M
