400e6fda2ee47064c6c910dc0c1d064bdedd636d
Lewis Russell <lewis6991@gmail.com>
2021-02-05 18:54:52 +0000

    Refactor git operations out of gitsigns.tl

--- lua/gitsigns/debug.lua
+++ lua/gitsigns/debug.lua
@@ -1,7 +1,12 @@
-local M = {}
+local M = {
+   debug_mode = false,
+}
 
 function M.dprint(msg, bufnr, caller)
-   local name = caller or debug.getinfo(2, 'n').name or ''
+   if not M.debug_mode then
+      return
+   end
+   local name = caller or debug.getinfo(1, 'n').name or ''
    print(string.format('%s(%s): %s\n', name, bufnr, msg))
 end
 
