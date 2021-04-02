7deca6c47f7e7ffc10161481b8e3f3b01ead6b6f
Lewis Russell <lewis6991@gmail.com>
2021-04-02 16:18:03 +0100

    Migrate to plenary's async lib

--- lua/gitsigns/async.lua
+++ lua/gitsigns/async.lua
@@ -1,3 +1,6 @@
+local a = require('plenary/async_lib/async')
+
+local M = {}
 
 
 
@@ -36,69 +39,178 @@
 
 
 
-local cb_function = {}
-local async_function = {}
 
-local co = coroutine
 
-local M = {
-   cb_function = cb_function,
-   async_function = async_function,
-}
 
-function M.async(func)
-   return function(...)
-      local params = { ... }
-      return function(callback)
-         local thread = co.create(func)
-         local function step(...)
-            local stat, ret = co.resume(thread, ...)
-            assert(stat, ret)
-            if co.status(thread) == "dead" then
-               if callback then
-                  callback(ret)
-               end
-            else
-               (ret)(step)
-            end
-         end
-         step(unpack(params))
-      end
-   end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+M.void = a.void
+M.void_async = function(func)
+   return M.void(a.async(func))
 end
 
-
-function M.sync(func)
-   return function(...)
-      M.async(func)(...)()
-   end
+M.await0 = a.await
+M.await1 = a.await
+M.await2 = a.await
+M.await3 = a.await
+M.await4 = a.await
+M.await_main = function()
+   a.await(a.scheduler())
 end
 
-
-function M.arun(func)
-   return function(...)
-      func(...)()
-   end
+M.wrap0 = function(func)
+   return a.wrap(func, 1)
+end
+M.wrap0_1 = function(func)
+   return a.wrap(func, 1)
+end
+M.wrap0_2 = function(func)
+   return a.wrap(func, 1)
+end
+M.wrap0_3 = function(func)
+   return a.wrap(func, 1)
+end
+M.wrap0_4 = function(func)
+   return a.wrap(func, 1)
+end
+M.wrap1 = function(func)
+   return a.wrap(func, 2)
+end
+M.wrap1_1 = function(func)
+   return a.wrap(func, 2)
+end
+M.wrap1_2 = function(func)
+   return a.wrap(func, 2)
+end
+M.wrap1_3 = function(func)
+   return a.wrap(func, 2)
+end
+M.wrap1_4 = function(func)
+   return a.wrap(func, 2)
+end
+M.wrap2 = function(func)
+   return a.wrap(func, 3)
+end
+M.wrap2_1 = function(func)
+   return a.wrap(func, 3)
+end
+M.wrap2_2 = function(func)
+   return a.wrap(func, 3)
+end
+M.wrap2_3 = function(func)
+   return a.wrap(func, 3)
+end
+M.wrap2_4 = function(func)
+   return a.wrap(func, 3)
+end
+M.wrap3 = function(func)
+   return a.wrap(func, 4)
+end
+M.wrap3_1 = function(func)
+   return a.wrap(func, 4)
+end
+M.wrap3_2 = function(func)
+   return a.wrap(func, 4)
+end
+M.wrap3_3 = function(func)
+   return a.wrap(func, 4)
+end
+M.wrap3_4 = function(func)
+   return a.wrap(func, 4)
+end
+M.wrap4 = function(func)
+   return a.wrap(func, 5)
+end
+M.wrap4_1 = function(func)
+   return a.wrap(func, 5)
+end
+M.wrap4_2 = function(func)
+   return a.wrap(func, 5)
+end
+M.wrap4_3 = function(func)
+   return a.wrap(func, 5)
+end
+M.wrap4_4 = function(func)
+   return a.wrap(func, 5)
 end
 
-function M.awrap(func)
-   return function(...)
-      local params = { ... }
-      return function(step)
-         table.insert(params, step)
-         return func(unpack(params))
-      end
-   end
-end
-
-function M.await(defer, ...)
-   return co.yield(defer(...))
-end
-
-function M.await_main()
-   if vim.in_fast_event() then
-      co.yield(vim.schedule)
-   end
-end
+M.async0 = a.async
+M.async0_1 = a.async
+M.async0_2 = a.async
+M.async0_3 = a.async
+M.async0_4 = a.async
+M.async1 = a.async
+M.async1_1 = a.async
+M.async1_2 = a.async
+M.async1_3 = a.async
+M.async1_4 = a.async
+M.async2 = a.async
+M.async2_1 = a.async
+M.async2_2 = a.async
+M.async2_3 = a.async
+M.async2_4 = a.async
+M.async3 = a.async
+M.async3_1 = a.async
+M.async3_2 = a.async
+M.async3_3 = a.async
+M.async3_4 = a.async
+M.async4 = a.async
+M.async4_1 = a.async
+M.async4_2 = a.async
+M.async4_3 = a.async
+M.async4_4 = a.async
 
 return M
--- lua/gitsigns/git.lua
+++ lua/gitsigns/git.lua
@@ -1,16 +1,26 @@
-local cb_function = require('gitsigns/async').cb_function
-
+local a = require('gitsigns/async')
 local gsd = require("gitsigns/debug")
 local util = require('gitsigns/util')
-local run_job = util.run_job
 
-local parse_diff_line = require("gitsigns/hunks").parse_diff_line
-local Hunk = require('gitsigns/hunks').Hunk
+local hunks = require("gitsigns/hunks")
+local Hunk = hunks.Hunk
 
 local uv = vim.loop
 local startswith = vim.startswith
 
 local M = {BlameInfo = {}, Version = {}, }
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -64,130 +74,143 @@ local function check_version(version)
    return true
 end
 
-function M.file_info(file, toplevel)
-   return function(callback)
-      local relpath
-      local object_name
-      local mode_bits
-      local stage
-      local has_conflict = false
-      run_job({
-         command = 'git',
-         args = {
-            '--no-pager',
-            'ls-files',
-            '--stage',
-            '--others',
-            '--exclude-standard',
-            file,
-         },
-         cwd = toplevel,
-         on_stdout = function(_, line)
-            local parts = vim.split(line, '\t')
-            if #parts > 1 then
-               relpath = parts[2]
-               local attrs = vim.split(parts[1], '%s+')
-               stage = tonumber(attrs[3])
-               if stage <= 1 then
-                  mode_bits = attrs[1]
-                  object_name = attrs[2]
-               else
-                  has_conflict = true
-               end
+M.file_info = a.wrap2_4(function(
+   file,
+   toplevel,
+   callback)
+
+   local relpath
+   local object_name
+   local mode_bits
+   local stage
+   local has_conflict = false
+   util.run_job({
+      command = 'git',
+      args = {
+         '--no-pager',
+         'ls-files',
+         '--stage',
+         '--others',
+         '--exclude-standard',
+         file,
+      },
+      cwd = toplevel,
+      on_stdout = function(_, line)
+         local parts = vim.split(line, '\t')
+         if #parts > 1 then
+            relpath = parts[2]
+            local attrs = vim.split(parts[1], '%s+')
+            stage = tonumber(attrs[3])
+            if stage <= 1 then
+               mode_bits = attrs[1]
+               object_name = attrs[2]
             else
-               relpath = parts[1]
+               has_conflict = true
             end
-         end,
-         on_exit = function()
-            callback(relpath, object_name, mode_bits, has_conflict)
-         end,
-      })
-   end
-end
+         else
+            relpath = parts[1]
+         end
+      end,
+      on_exit = function()
+         callback(relpath, object_name, mode_bits, has_conflict)
+      end,
+   })
+end)
 
-function M.get_staged(toplevel, relpath, stage, output)
-   return function(callback)
+M.get_staged = a.wrap4(function(
+   toplevel,
+   relpath,
+   stage,
+   output,
+   callback)
 
 
-      local outf = io.open(output, 'wb')
-      run_job({
-         command = 'git',
-         args = {
-            '--no-pager',
-            'show',
-            ':' .. tostring(stage) .. ':' .. relpath,
-         },
-         cwd = toplevel,
-         on_stdout = function(_, line)
-            outf:write(line)
-            outf:write('\n')
-         end,
-         on_exit = function()
-            outf:close()
-            callback()
-         end,
-      })
-   end
-end
 
-function M.get_staged_text(toplevel, relpath, stage)
-   return function(callback)
-      local result = {}
-      run_job({
-         command = 'git',
-         args = {
-            '--no-pager',
-            'show',
-            ':' .. tostring(stage) .. ':' .. relpath,
-         },
-         cwd = toplevel,
-         on_stdout = function(_, line)
-            table.insert(result, line)
-         end,
-         on_exit = function()
-            callback(result)
-         end,
-      })
-   end
-end
+   local outf = io.open(output, 'wb')
+   util.run_job({
+      command = 'git',
+      args = {
+         '--no-pager',
+         'show',
+         ':' .. tostring(stage) .. ':' .. relpath,
+      },
+      cwd = toplevel,
+      on_stdout = function(_, line)
+         outf:write(line)
+         outf:write('\n')
+      end,
+      on_exit = function()
+         outf:close()
+         callback()
+      end,
+   })
+end)
 
-function M.run_blame(file, toplevel, lines, lnum)
-   return function(callback)
-      local results = {}
-      run_job({
-         command = 'git',
-         args = {
-            '--no-pager',
-            'blame',
-            '--contents', '-',
-            '-L', lnum .. ',+1',
-            '--line-porcelain',
-            file,
-         },
-         writer = lines,
-         cwd = toplevel,
-         on_stdout = function(_, line)
-            table.insert(results, line)
-         end,
-         on_exit = function()
-            local ret = {}
-            local header = vim.split(table.remove(results, 1), ' ')
-            ret.sha = header[1]
-            ret.abbrev_sha = string.sub(ret.sha, 1, 8)
-            ret.orig_lnum = tonumber(header[2])
-            ret.final_lnum = tonumber(header[3])
-            for _, l in ipairs(results) do
-               if not startswith(l, '\t') then
-                  local cols = vim.split(l, ' ')
-                  local key = table.remove(cols, 1):gsub('-', '_')
-                  ret[key] = table.concat(cols, ' ')
-               end
+M.get_staged_text = a.wrap3_1(function(
+   toplevel,
+   relpath,
+   stage,
+   callback)
+
+   local result = {}
+   util.run_job({
+      command = 'git',
+      args = {
+         '--no-pager',
+         'show',
+         ':' .. tostring(stage) .. ':' .. relpath,
+      },
+      cwd = toplevel,
+      on_stdout = function(_, line)
+         table.insert(result, line)
+      end,
+      on_exit = function()
+         callback(result)
+      end,
+   })
+end)
+
+M.run_blame = a.wrap4_1(function(
+   file,
+   toplevel,
+   lines,
+   lnum,
+   callback)
+
+   local results = {}
+   util.run_job({
+      command = 'git',
+      args = {
+         '--no-pager',
+         'blame',
+         '--contents', '-',
+         '-L', lnum .. ',+1',
+         '--line-porcelain',
+         file,
+      },
+      writer = lines,
+      cwd = toplevel,
+      on_stdout = function(_, line)
+         table.insert(results, line)
+      end,
+      on_exit = function()
+         local ret = {}
+         local header = vim.split(table.remove(results, 1), ' ')
+         ret.sha = header[1]
+         ret.abbrev_sha = string.sub(ret.sha, 1, 8)
+         ret.orig_lnum = tonumber(header[2])
+         ret.final_lnum = tonumber(header[3])
+         for _, l in ipairs(results) do
+            if not startswith(l, '\t') then
+               local cols = vim.split(l, ' ')
+               local key = table.remove(cols, 1):gsub('-', '_')
+               ret[key] = table.concat(cols, ' ')
             end
-            callback(ret)
-         end,
-      })
-   end
-end
+         end
+         callback(ret)
+      end,
+   })
+end)
 
 local function process_abbrev_head(gitdir, head_str)
    if not gitdir then
@@ -206,109 +229,110 @@ local function process_abbrev_head(gitdir, head_str)
    return head_str
 end
 
-function M.get_repo_info(path)
-   return function(callback)
-      local out = {}
+M.get_repo_info = a.wrap1_3(function(
+   path, callback)
+   local out = {}
 
 
 
-      local has_abs_gd = check_version({ 2, 13 })
-      local git_dir_opt = has_abs_gd and '--absolute-git-dir' or '--git-dir'
+   local has_abs_gd = check_version({ 2, 13 })
+   local git_dir_opt = has_abs_gd and '--absolute-git-dir' or '--git-dir'
 
-      run_job({
-         command = 'git',
-         args = { 'rev-parse',
+   util.run_job({
+      command = 'git',
+      args = { 'rev-parse',
 '--show-toplevel',
 git_dir_opt,
 '--abbrev-ref', 'HEAD',
-         },
-         cwd = path,
-         on_stdout = function(_, line)
-            if not has_abs_gd and #out == 1 then
-               line = uv.fs_realpath(line)
-            end
-            table.insert(out, line)
-         end,
-         on_exit = vim.schedule_wrap(function()
-            local toplevel = out[1]
-            local gitdir = out[2]
-            local abbrev_head = process_abbrev_head(gitdir, out[3])
-            callback(toplevel, gitdir, abbrev_head)
-         end),
-      })
-   end
-end
+      },
+      cwd = path,
+      on_stdout = function(_, line)
+         if not has_abs_gd and #out == 1 then
+            line = uv.fs_realpath(line)
+         end
+         table.insert(out, line)
+      end,
+      on_exit = vim.schedule_wrap(function()
+         local toplevel = out[1]
+         local gitdir = out[2]
+         local abbrev_head = process_abbrev_head(gitdir, out[3])
+         callback(toplevel, gitdir, abbrev_head)
+      end),
+   })
+end)
 
-function M.stage_lines(toplevel, lines)
-   return function(callback)
-      local status = true
-      local err = {}
-      run_job({
-         command = 'git',
-         args = { 'apply', '--cached', '--unidiff-zero', '-' },
-         cwd = toplevel,
-         writer = lines,
-         on_stderr = function(_, line)
-            status = false
-            table.insert(err, line)
-         end,
-         on_exit = function()
-            if not status then
-               local s = table.concat(err, '\n')
-               error('Cannot stage lines. Command stderr:\n\n' .. s)
-            end
-            callback()
-         end,
-      })
-   end
-end
+M.stage_lines = a.wrap2(function(
+   toplevel, lines, callback)
+   local status = true
+   local err = {}
+   util.run_job({
+      command = 'git',
+      args = { 'apply', '--cached', '--unidiff-zero', '-' },
+      cwd = toplevel,
+      writer = lines,
+      on_stderr = function(_, line)
+         status = false
+         table.insert(err, line)
+      end,
+      on_exit = function()
+         if not status then
+            local s = table.concat(err, '\n')
+            error('Cannot stage lines. Command stderr:\n\n' .. s)
+         end
+         callback()
+      end,
+   })
+end)
 
-function M.add_file(toplevel, file)
-   return function(callback)
-      local status = true
-      local err = {}
-      run_job({
-         command = 'git',
-         args = { 'add', '--intent-to-add', file },
-         cwd = toplevel,
-         on_stderr = function(_, line)
-            status = false
-            table.insert(err, line)
-         end,
-         on_exit = function()
-            if not status then
-               local s = table.concat(err, '\n')
-               error('Cannot add file. Command stderr:\n\n' .. s)
-            end
-            callback()
-         end,
-      })
-   end
-end
+M.add_file = a.wrap2(function(
+   toplevel, file, callback)
+   local status = true
+   local err = {}
+   util.run_job({
+      command = 'git',
+      args = { 'add', '--intent-to-add', file },
+      cwd = toplevel,
+      on_stderr = function(_, line)
+         status = false
+         table.insert(err, line)
+      end,
+      on_exit = function()
+         if not status then
+            local s = table.concat(err, '\n')
+            error('Cannot add file. Command stderr:\n\n' .. s)
+         end
+         callback()
+      end,
+   })
+end)
 
-function M.update_index(toplevel, mode_bits, object_name, file)
-   return function(callback)
-      local status = true
-      local err = {}
-      local cacheinfo = table.concat({ mode_bits, object_name, file }, ',')
-      run_job({
-         command = 'git',
-         args = { 'update-index', '--add', '--cacheinfo', cacheinfo },
-         cwd = toplevel,
-         on_stderr = function(_, line)
-            status = false
-            table.insert(err, line)
-         end,
-         on_exit = function()
-            if not status then
-               local s = table.concat(err, '\n')
-               error('Cannot update index. Command stderr:\n\n' .. s)
-            end
-            callback()
-         end,
-      })
-   end
-end
+M.update_index = a.wrap4(function(
+   toplevel,
+   mode_bits,
+   object_name,
+   file,
+   callback)
+
+   local status = true
+   local err = {}
+   local cacheinfo = table.concat({ mode_bits, object_name, file }, ',')
+   util.run_job({
+      command = 'git',
+      args = { 'update-index', '--add', '--cacheinfo', cacheinfo },
+      cwd = toplevel,
+      on_stderr = function(_, line)
+         status = false
+         table.insert(err, line)
+      end,
+      on_exit = function()
+         if not status then
+            local s = table.concat(err, '\n')
+            error('Cannot update index. Command stderr:\n\n' .. s)
+         end
+         callback()
+      end,
+   })
+end)
 
 local function write_to_file(path, text)
    local f = io.open(path, 'wb')
@@ -319,18 +343,16 @@ local function write_to_file(path, text)
    f:close()
 end
 
-function M.run_diff(staged, text, diff_algo)
-   return function(callback)
-      local results = {}
+M.run_diff = a.wrap3_1(function(
+   staged,
+   text,
+   diff_algo,
+   callback)
 
-      local buffile = staged .. '_buf'
-      write_to_file(buffile, text)
+   local results = {}
 
-
-
-
-
-
+   local buffile = staged .. '_buf'
+   write_to_file(buffile, text)
 
 
 
@@ -342,95 +364,95 @@ function M.run_diff(staged, text, diff_algo)
 
 
 
-      run_job({
-         command = 'git',
-         args = {
-            '--no-pager',
-            '-c', 'core.safecrlf=false',
-            'diff',
-            '--color=never',
-            '--diff-algorithm=' .. diff_algo,
-            '--patch-with-raw',
-            '--unified=0',
-            staged,
-            buffile,
-         },
-         on_stdout = function(_, line)
-            if startswith(line, '@@') then
-               table.insert(results, parse_diff_line(line))
-            else
-               if #results > 0 then
-                  table.insert(results[#results].lines, line)
-               end
+
+
+
+
+
+
+   util.run_job({
+      command = 'git',
+      args = {
+         '--no-pager',
+         '-c', 'core.safecrlf=false',
+         'diff',
+         '--color=never',
+         '--diff-algorithm=' .. diff_algo,
+         '--patch-with-raw',
+         '--unified=0',
+         staged,
+         buffile,
+      },
+      on_stdout = function(_, line)
+         if startswith(line, '@@') then
+            table.insert(results, hunks.parse_diff_line(line))
+         else
+            if #results > 0 then
+               table.insert(results[#results].lines, line)
             end
-         end,
-         on_stderr = function(err, line)
-            if err then
-               gsd.eprint(err)
-            end
-            if line then
-               gsd.eprint(line)
-            end
-         end,
-         on_exit = function()
-            os.remove(buffile)
-            callback(results)
-         end,
-      })
+         end
+      end,
+      on_stderr = function(err, line)
+         if err then
+            gsd.eprint(err)
+         end
+         if line then
+            gsd.eprint(line)
+         end
+      end,
+      on_exit = function()
+         os.remove(buffile)
+         callback(results)
+      end,
+   })
+end)
+
+M.set_version = a.wrap1(function(version, callback)
+   if version ~= 'auto' then
+      M.version = parse_version(version)
+      callback()
+      return
    end
-end
-
-function M.set_version(version)
-   return function(callback)
-      if version ~= 'auto' then
-         M.version = parse_version(version)
+   util.run_job({
+      command = 'git', args = { '--version' },
+      on_stdout = function(_, line)
+         assert(startswith(line, 'git version'), 'Unexpected output: ' .. line)
+         local parts = vim.split(line, '%s+')
+         M.version = parse_version(parts[3])
+      end,
+      on_stderr = function(err, line)
+         if err then
+            gsd.eprint(err)
+         end
+         if line then
+            gsd.eprint(line)
+         end
+      end,
+      on_exit = function()
          callback()
-         return
-      end
-      run_job({
-         command = 'git', args = { '--version' },
-         on_stdout = function(_, line)
-            assert(startswith(line, 'git version'), 'Unexpected output: ' .. line)
-            local parts = vim.split(line, '%s+')
-            M.version = parse_version(parts[3])
-         end,
-         on_stderr = function(err, line)
-            if err then
-               gsd.eprint(err)
-            end
-            if line then
-               gsd.eprint(line)
-            end
-         end,
-         on_exit = function()
-            callback()
-         end,
-      })
-   end
-end
+      end,
+   })
+end)
 
-function M.command(...)
-   local args = { ... }
-   return function(callback)
-      local result = {}
-      run_job({
-         command = 'git', args = args,
-         on_stdout = function(_, line)
-            table.insert(result, line)
-         end,
-         on_stderr = function(err, line)
-            if err then
-               gsd.eprint(err)
-            end
-            if line then
-               gsd.eprint(line)
-            end
-         end,
-         on_exit = function()
-            callback(result)
-         end,
-      })
-   end
-end
+M.command = a.wrap1_1(function(args, callback)
+   local result = {}
+   util.run_job({
+      command = 'git', args = args,
+      on_stdout = function(_, line)
+         table.insert(result, line)
+      end,
+      on_stderr = function(err, line)
+         if err then
+            gsd.eprint(err)
+         end
+         if line then
+            gsd.eprint(line)
+         end
+      end,
+      on_exit = function()
+         callback(result)
+      end,
+   })
+end)
 
 return M
