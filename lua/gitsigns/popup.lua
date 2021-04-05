98305f725d9d96ac1fb38f441f6ae8ca2be08763
Lewis Russell <lewis6991@gmail.com>
2021-04-05 09:49:10 +0100

    Preview: Add border and use minimal style
    - I nvim doesn't yet have border support, then gitsigns should fallback to
    opening a window without a border.
    
    - Also fix #126

--- lua/gitsigns/popup.lua
+++ lua/gitsigns/popup.lua
@@ -13,6 +13,19 @@ local function calc_width(lines)
    return width
 end
 
+
+local function open_win(bufnr, enter, opts)
+   local stat, win_id = pcall(api.nvim_open_win, bufnr, enter, opts)
+
+   if not stat then
+
+      opts.border = nil
+      win_id = api.nvim_open_win(bufnr, enter, opts)
+   end
+
+   return win_id
+end
+
 local function bufnr_calc_width(buf, lines)
    return api.nvim_buf_call(buf, function()
       return calc_width(lines)
@@ -33,19 +46,19 @@ function popup.create(what, opts)
       api.nvim_buf_set_option(bufnr, 'tabstop', opts.tabstop)
    end
 
-   local win_id = api.nvim_open_win(bufnr, false, {
-      relative = opts.relative,
+   local win_id = open_win(bufnr, false, {
+      border = 'single',
+      style = 'minimal',
+      relative = opts.relative or 'cursor',
       row = opts.row or 0,
-      col = opts.col or 0,
+      col = opts.col or 1,
       height = opts.height or #what,
       width = opts.width or bufnr_calc_width(bufnr, what),
    })
 
    vim.lsp.util.close_preview_autocmd({ 'CursorMoved', 'CursorMovedI' }, win_id)
 
-   if opts.highlight then
-      api.nvim_win_set_option(win_id, 'winhl', string.format('Normal:%s', opts.highlight))
-   end
+   api.nvim_win_set_option(win_id, 'winhl', string.format('NormalFloat:Normal'))
 
    return win_id, bufnr
 end
