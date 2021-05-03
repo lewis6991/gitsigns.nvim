19cadcafb3c1f36de215b2aad81480263735af98
Lewis Russell <lewis6991@gmail.com>
2021-05-03 11:09:35 +0100

    Simplify floating window creation
    - config.preview_config now contains the true defaults
    - No longer apply additional window highlights
    - Refactor and simplify
    
    Resolves #170

--- lua/gitsigns/popup.lua
+++ lua/gitsigns/popup.lua
@@ -2,62 +2,35 @@ local popup = {}
 
 local api = vim.api
 
-local function calc_width(lines)
-   local width = 0
-   for _, l in ipairs(lines) do
-      local len = vim.fn.strdisplaywidth(l)
-      if len > width then
-         width = len
-      end
-   end
-   return width
-end
-
-
-local function open_win(bufnr, enter, opts)
-   local stat, win_id = pcall(api.nvim_open_win, bufnr, enter, opts)
-
-   if not stat then
-
-      opts.border = nil
-      win_id = api.nvim_open_win(bufnr, enter, opts)
-   elseif opts.border then
-      api.nvim_win_set_option(win_id, 'winhl', string.format('NormalFloat:Normal'))
-   end
-
-   return win_id
-end
-
 local function bufnr_calc_width(buf, lines)
    return api.nvim_buf_call(buf, function()
-      return calc_width(lines)
+      local width = 0
+      for _, l in ipairs(lines) do
+         local len = vim.fn.strdisplaywidth(l)
+         if len > width then
+            width = len
+         end
+      end
+      return width
    end)
 end
 
-function popup.create(what, opts)
+function popup.create(lines, opts)
    local ts = api.nvim_buf_get_option(0, 'tabstop')
    local bufnr = api.nvim_create_buf(false, true)
    assert(bufnr, "Failed to create buffer")
 
-   api.nvim_buf_set_lines(bufnr, 0, -1, true, what)
-
-   opts = opts or {}
+   api.nvim_buf_set_lines(bufnr, 0, -1, true, lines)
 
 
 
-   if opts.tabstop then
-      api.nvim_buf_set_option(bufnr, 'tabstop', ts)
-   end
+   api.nvim_buf_set_option(bufnr, 'tabstop', ts)
 
-   local win_id = open_win(bufnr, false, {
-      border = opts.border or 'single',
-      style = opts.style or 'minimal',
-      relative = opts.relative or 'cursor',
-      row = opts.row or 0,
-      col = opts.col or 1,
-      height = opts.height or #what,
-      width = opts.width or bufnr_calc_width(bufnr, what),
-   })
+   local opts1 = vim.deepcopy(opts or {})
+   opts1.height = opts1.height or #lines
+   opts1.width = opts1.width or bufnr_calc_width(bufnr, lines)
+
+   local win_id = api.nvim_open_win(bufnr, false, opts1)
 
    vim.lsp.util.close_preview_autocmd({ 'CursorMoved', 'CursorMovedI' }, win_id)
 
