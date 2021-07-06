e0e961e306af3f8ac96b6aca852a86475ecf284c
Lewis Russell <lewis6991@gmail.com>
2021-07-06 11:27:57 +0100

    Tweak how derived highlights are applied
    If setup() is called before VimEnter and apply highlights in a VimEnter
    autocmd, otherwise just apply them.

--- lua/gitsigns/highlight.lua
+++ lua/gitsigns/highlight.lua
@@ -66,7 +66,14 @@ M.setup_highlight = function(hl_name)
    for _, d in ipairs(hls[hl_name]) do
       if is_hl_set(d) then
          dprint(('Deriving %s from %s'):format(hl_name, d))
-         vim.cmd(('highlight link %s %s'):format(hl_name, d))
+         local hl_cmd = ('highlight link %s %s'):format(hl_name, d)
+
+
+         if vim.v.vim_did_enter == 1 then
+            vim.cmd(hl_cmd)
+         else
+            vim.cmd(('autocmd VimEnter * ++once %s'):format(hl_cmd))
+         end
          return
       end
    end
