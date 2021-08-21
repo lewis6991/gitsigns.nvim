7268559884e63b1d45470a5723c10cd128a0b93e
Lewis Russell <lewis6991@gmail.com>
2021-08-21 12:33:52 +0100

    Add config._verbose
    To help diagnose issues with malformed process output

--- lua/gitsigns/debug.lua
+++ lua/gitsigns/debug.lua
@@ -1,16 +1,18 @@
 local M = {
    debug_mode = false,
+   verbose = false,
    messages = {},
 }
 
-local function getvarvalue(name)
+local function getvarvalue(name, lvl)
+   lvl = lvl + 1
    local value
    local found
 
 
    local i = 1
    while true do
-      local n, v = debug.getlocal(3, i)
+      local n, v = debug.getlocal(lvl, i)
       if not n then break end
       if n == name then
          value = v
@@ -21,7 +23,7 @@ local function getvarvalue(name)
    if found then return value end
 
 
-   local func = debug.getinfo(3).func
+   local func = debug.getinfo(lvl).func
    i = 1
    while true do
       local n, v = debug.getupvalue(func, i)
@@ -34,29 +36,54 @@ local function getvarvalue(name)
    return getfenv(func)[name]
 end
 
+local function get_context(lvl)
+   lvl = lvl + 1
+   local ret = {}
+   ret.name = getvarvalue('__FUNC__', lvl)
+   if not ret.name then
+      local name0 = debug.getinfo(lvl, 'n').name or ''
+      ret.name = name0:gsub('(.*)%d+$', '%1')
+   end
+   ret.bufnr = getvarvalue('bufnr', lvl) or
+   getvarvalue('cbuf', lvl) or
+   getvarvalue('buf', lvl)
+
+   return ret
+end
 
 
-function M.dprint(obj, ...)
-   if not M.debug_mode then
-      return
-   end
-   local msg = type(obj) == "string" and obj:format(...) or vim.inspect(obj)
-   local name = getvarvalue('__FUNC__')
-   if not name then
-      local name0 = debug.getinfo(2, 'n').name or ''
-      name = name0:gsub('(.*)%d+$', '%1')
-   end
+
+local function cprint(obj, lvl)
+   lvl = lvl + 1
+   local msg = type(obj) == "string" and obj or vim.inspect(obj)
+   local ctx = get_context(lvl)
    local msg2
-
-   local bufnr = getvarvalue('bufnr') or
-   getvarvalue('cbuf') or
-   getvarvalue('buf')
-   if bufnr then
-      msg2 = string.format('%s(%s): %s', name, bufnr, msg)
+   if ctx.bufnr then
+      msg2 = string.format('%s(%s): %s', ctx.name, ctx.bufnr, msg)
    else
-      msg2 = string.format('%s: %s', name, msg)
+      msg2 = string.format('%s: %s', ctx.name, msg)
    end
    table.insert(M.messages, msg2)
+end
+
+function M.dprint(obj)
+   if not M.debug_mode then return end
+   cprint(obj, 2)
+end
+
+function M.dprintf(obj, ...)
+   if not M.debug_mode then return end
+   cprint(obj:format(...), 2)
+end
+
+function M.vprint(obj)
+   if not (M.debug_mode and M.verbose) then return end
+   cprint(obj, 2)
+end
+
+function M.vprintf(obj, ...)
+   if not (M.debug_mode and M.verbose) then return end
+   cprint(obj:format(...), 2)
 end
 
 function M.eprint(msg)
