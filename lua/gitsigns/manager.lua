fe33dcc0e31c40f9c9b1994e7acc9e5895d9d0d3
Saecki <tobiasschmitz2001@gmail.com>
2021-09-07 17:17:17 +0200

    undo changes in manager.tl

--- lua/gitsigns/manager.lua
+++ lua/gitsigns/manager.lua
@@ -256,20 +256,20 @@ end
 
 
 do
-   local running = {}
+   local running = false
    local scheduled = {}
    M.update = function(bufnr, bcache)
       scheduled[bufnr] = true
-      if not running[bufnr] then
-         running[bufnr] = true
+      if not running then
+         running = true
          while scheduled[bufnr] do
             scheduled[bufnr] = nil
             update0(bufnr, bcache)
          end
-         running[bufnr] = false
+         running = false
       else
 
-         while running[bufnr] do
+         while running do
             async_util.sleep(100)
          end
       end
