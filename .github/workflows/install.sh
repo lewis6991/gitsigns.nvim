270934cf36b9c70fac01f717f5eeb5f039b9b35f
Lewis Russell <lewis6991@gmail.com>
2021-12-13 13:19:51 +0000

    fix

---
+++ .github/workflows/install.sh
@@ -0,0 +1,22 @@
+sudo apt update
+sudo apt install -y \
+  autoconf \
+  automake \
+  cmake \
+  g++ \
+  gettext \
+  gperf \
+  libjemalloc-dev \
+  libluajit-5.1-dev \
+  libmsgpack-dev \
+  libtermkey-dev \
+  libtool \
+  libtool-bin \
+  libunibilium-dev \
+  libvterm-dev \
+  lua-bitop \
+  lua-lpeg \
+  lua-mpack \
+  ninja-build \
+  pkg-config \
+  unzip
