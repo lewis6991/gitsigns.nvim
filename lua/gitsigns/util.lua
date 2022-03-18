3791dfa1ee356a3250e0b74f63bad90e27455f60
Lewis Russell <lewis6991@gmail.com>
2022-03-18 11:21:36 +0000

    fix(dos): don't insert extra carriage returns
    Fixes #492

--- lua/gitsigns/util.lua
+++ lua/gitsigns/util.lua
@@ -45,6 +45,15 @@ function M.buf_lines(bufnr)
    return buftext
 end
 
+function M.set_lines(bufnr, start_row, end_row, lines)
+   if vim.bo[bufnr].fileformat == 'dos' then
+      for i = 1, #lines do
+         lines[i] = lines[i]:gsub('\r$', '')
+      end
+   end
+   vim.api.nvim_buf_set_lines(bufnr, start_row, end_row, false, lines)
+end
+
 function M.tmpname()
    if is_unix then
       return os.tmpname()
