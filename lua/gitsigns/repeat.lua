c3c4cdd5832fb03cc10bce8b24166cc509867225
Lewis Russell <lewis6991@gmail.com>
2022-09-15 17:58:39 +0100

    feat(repeat): better implementation

--- lua/gitsigns/repeat.lua
+++ lua/gitsigns/repeat.lua
@@ -1,35 +1,29 @@
 local api = vim.api
+
 local M = {}
 
 
 
-local repeat_fn
 
-function M.repeat_action()
-   repeat_fn()
-end
 
 function M.mk_repeatable(fn)
    return function(...)
       local args = { ... }
       local nargs = select('#', ...)
-      repeat_fn = function()
+      vim.go.operatorfunc = "v:lua.require'gitsigns.repeat'.repeat_action"
+
+      M.repeat_action = function()
          fn(unpack(args, 1, nargs))
-         local vimfn = vim.fn
-         local sequence = string.format('%sGitsignsRepeat',
-         api.nvim_replace_termcodes('<Plug>', true, true, true))
-         pcall(vimfn['repeat#set'], sequence, -1)
+         if vim.fn.exists('*repeat#set') == 1 then
+            local action = api.nvim_replace_termcodes(
+            string.format('<cmd>call %s()<cr>', vim.go.operatorfunc),
+            true, true, true)
+            vim.fn['repeat#set'](action, -1)
+         end
       end
 
-      repeat_fn()
+      vim.cmd('normal g@l')
    end
 end
-
-api.nvim_set_keymap(
-'n',
-'<Plug>GitsignsRepeat',
-'<cmd>lua require"gitsigns.repeat".repeat_action()<CR>',
-{ noremap = false, silent = true })
-
 
 return M
