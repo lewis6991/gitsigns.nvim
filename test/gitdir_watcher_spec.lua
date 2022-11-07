0db342ada805b01c4a7ad79d5f8375c81d1b1412
Lewis Russell <lewis6991@gmail.com>
2022-11-07 16:31:46 +0000

    fix(follow_files): do not rename if buffer name already exists
    Fixes #571

--- test/gitdir_watcher_spec.lua
+++ test/gitdir_watcher_spec.lua
@@ -1,5 +1,7 @@
 local helpers = require('test.gs_helpers')
 
+local Screen = require('test.functional.ui.screen')
+
 local clear           = helpers.clear
 local exec_lua        = helpers.exec_lua
 local edit            = helpers.edit
@@ -17,6 +19,14 @@ local get_buf_name    = helpers.curbufmeths.get_name
 
 local it = helpers.it(it)
 
+local function get_bufs()
+  local bufs = {}
+  for _, b in ipairs(helpers.meths.list_bufs()) do
+    bufs[b.id] = helpers.meths.buf_get_name(b)
+  end
+  return bufs
+end
+
 describe('gitdir_watcher', function()
   before_each(function()
     clear()
@@ -31,10 +41,15 @@ describe('gitdir_watcher', function()
   end)
 
   it('can follow moved files', function()
+    local screen = Screen.new(20, 17)
+    screen:attach({ext_messages=false})
     setup_test_repo()
     setup_gitsigns(test_config)
     command('Gitsigns clear_debug')
     edit(test_file)
+
+    local test_file2 = test_file..'2'
+    local test_file3 = test_file..'3'
 
     match_debug_messages {
       'attach(1): Attaching (trigger=BufRead)',
@@ -46,9 +61,11 @@ describe('gitdir_watcher', function()
       'update(1): updates: 1, jobs: 6',
     }
 
+    eq({[1] = test_file}, get_bufs())
+
     command('Gitsigns clear_debug')
 
-    git{'mv', test_file, test_file..'2'}
+    git{'mv', test_file, test_file2}
 
     match_debug_messages {
       'watcher_cb(1): Git dir update',
@@ -56,47 +73,50 @@ describe('gitdir_watcher', function()
       p('run_job: git .* ls%-files .* '..test_file),
       p'run_job: git .* diff %-%-name%-status %-C %-%-cached',
       'handle_moved(1): File moved to dummy.txt2',
-      p('run_job: git .* ls%-files .* '..test_file..'2'),
+      p('run_job: git .* ls%-files .* '..test_file2),
+      p'handle_moved%(1%): Renamed buffer 1 from .*/dummy.txt to .*/dummy.txt2',
       p'run_job: git .* show :0:dummy.txt2',
       'update(1): updates: 2, jobs: 11'
     }
 
-    eq(test_file..'2', get_buf_name())
+    eq({[1] = test_file2}, get_bufs())
 
     command('Gitsigns clear_debug')
 
-    git{'mv', test_file..'2', test_file..'3'}
+    git{'mv', test_file2, test_file3}
 
     match_debug_messages {
       'watcher_cb(1): Git dir update',
       p'run_job: git .* rev%-parse %-%-show%-toplevel %-%-absolute%-git%-dir %-%-abbrev%-ref HEAD',
-      p('run_job: git .* ls%-files .* '..test_file..'2'),
+      p('run_job: git .* ls%-files .* '..test_file2),
       p'run_job: git .* diff %-%-name%-status %-C %-%-cached',
       'handle_moved(1): File moved to dummy.txt3',
-      p('run_job: git .* ls%-files .* '..test_file..'3'),
+      p('run_job: git .* ls%-files .* '..test_file3),
+      p'handle_moved%(1%): Renamed buffer 1 from .*/dummy.txt2 to .*/dummy.txt3',
       p'run_job: git .* show :0:dummy.txt3',
       'update(1): updates: 3, jobs: 16'
     }
 
-    eq(test_file..'3', get_buf_name())
+    eq({[1] = test_file3}, get_bufs())
 
     command('Gitsigns clear_debug')
 
-    git{'mv', test_file..'3', test_file}
+    git{'mv', test_file3, test_file}
 
     match_debug_messages {
       'watcher_cb(1): Git dir update',
       p'run_job: git .* rev%-parse %-%-show%-toplevel %-%-absolute%-git%-dir %-%-abbrev%-ref HEAD',
-      p('run_job: git .* ls%-files .* '..test_file..'3'),
+      p('run_job: git .* ls%-files .* '..test_file3),
       p'run_job: git .* diff %-%-name%-status %-C %-%-cached',
       p('run_job: git .* ls%-files .* '..test_file),
       'handle_moved(1): Moved file reset',
       p('run_job: git .* ls%-files .* '..test_file),
+      p'handle_moved%(1%): Renamed buffer 1 from .*/dummy.txt3 to .*/dummy.txt',
       p'run_job: git .* show :0:dummy.txt',
       'update(1): updates: 4, jobs: 22'
     }
 
-    eq(test_file, get_buf_name())
+    eq({[1] = test_file}, get_bufs())
 
   end)
 
