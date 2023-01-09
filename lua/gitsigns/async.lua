114362a85e51918ab2965181ffa31932c181f32f
Lewis Russell <lewis6991@gmail.com>
2023-01-09 16:55:45 +0000

    feat: yet another async implementation
    Migrate changes discussed in https://github.com/neovim/neovim/issues/19624

--- lua/gitsigns/async.lua
+++ lua/gitsigns/async.lua
@@ -16,10 +16,13 @@
 
 
 
+
+
 local M = {}
 
 
 
+local Async_T = {}
 
 
 
@@ -28,7 +31,124 @@ local M = {}
 
 
 
-local main_co_or_nil = coroutine.running()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+local handles = setmetatable({}, { __mode = 'k' })
+
+
+function M.running()
+   local current = coroutine.running()
+   if current and handles[current] then
+      return true
+   end
+end
+
+
+local function maxn(x)
+   return ((table).maxn)(x)
+end
+
+local function is_Async_T(handle)
+   if handle and
+      type(handle) == 'table' and
+      vim.is_callable(handle.cancel) and
+      vim.is_callable(handle.is_cancelled) then
+      return true
+   end
+end
+
+
+function Async_T:cancel(cb)
+
+   if self._current and not self._current:is_cancelled() then
+      self._current:cancel(cb)
+   end
+end
+
+function Async_T.new(co)
+   local handle = setmetatable({}, { __index = Async_T })
+   handles[co] = handle
+   return handle
+end
+
+
+function Async_T:is_cancelled()
+   return self._current and self._current:is_cancelled()
+end
+
+local function run(func, callback, ...)
+   local co = coroutine.create(func)
+   local handle = Async_T.new(co)
+
+   local function step(...)
+      local ret = { coroutine.resume(co, ...) }
+      local stat, nargs, err_or_fn = unpack(ret)
+
+      if not stat then
+         error(string.format("The coroutine failed with this message: %s\n%s",
+         err_or_fn, debug.traceback(co)))
+      end
+
+      if coroutine.status(co) == 'dead' then
+         if callback then
+            callback(unpack(ret, 4, maxn(ret)))
+         end
+         return
+      end
+
+      assert(type(err_or_fn) == 'function', "type error :: expected func")
+
+      local args = { select(4, unpack(ret)) }
+      args[nargs] = step
+
+      local r = err_or_fn(unpack(args, 1, nargs))
+      if is_Async_T(r) then
+         handle._current = r
+      end
+   end
+
+   step(...)
+   return handle
+end
+
+local function wait(argc, func, ...)
+
+
+   local function pfunc(...)
+      local args = { ... }
+      local cb = args[argc]
+      args[argc] = function(...)
+         cb(true, ...)
+      end
+      xpcall(func, function(err)
+         cb(false, err, debug.traceback())
+      end, unpack(args, 1, argc))
+   end
+
+   local ret = { coroutine.yield(argc, pfunc, ...) }
+
+   local ok = ret[1]
+   if not ok then
+      local _, err, traceback = unpack(ret)
+      error(string.format("Wrapped function failed: %s\n%s", err, traceback))
+   end
+
+   return unpack(ret, 2, maxn(ret))
+end
 
 
 
@@ -37,10 +157,25 @@ local main_co_or_nil = coroutine.running()
 function M.wrap(func, argc)
    assert(argc)
    return function(...)
-      if coroutine.running() == main_co_or_nil then
+      if not M.running() then
          return func(...)
       end
-      return coroutine.yield(func, argc, ...)
+      return wait(argc, func, ...)
+   end
+end
+
+
+
+
+
+function M.create(func, argc)
+   argc = argc or 0
+   return function(...)
+      if M.running() then
+         return func(...)
+      end
+      local callback = select(argc + 1, ...)
+      return run(func, callback, unpack({ ... }, 1, argc))
    end
 end
 
@@ -50,33 +185,10 @@ end
 
 function M.void(func)
    return function(...)
-      if coroutine.running() ~= main_co_or_nil then
+      if M.running() then
          return func(...)
       end
-
-      local co = coroutine.create(func)
-
-      local function step(...)
-         local ret = { coroutine.resume(co, ...) }
-         local stat, err_or_fn, nargs = unpack(ret)
-
-         if not stat then
-            error(string.format("The coroutine failed with this message: %s\n%s",
-            err_or_fn, debug.traceback(co)))
-         end
-
-         if coroutine.status(co) == 'dead' then
-            return
-         end
-
-         assert(type(err_or_fn) == "function", "type error :: expected func")
-
-         local args = { select(4, unpack(ret)) }
-         args[nargs] = step
-         err_or_fn(unpack(args, 1, nargs))
-      end
-
-      step(...)
+      return run(func, nil, ...)
    end
 end
 
