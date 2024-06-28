lewis6991/gitsigns.nvim

commit 8df63f2ddc615feb71fd4aee45a4cee022876df1

fix(blame): update current_line_blame when attaching

lua/gitsigns/current_line_blame.lua
@@ -207,50 +207,49 @@ end
 local update = async.create(1, debounce.throttle_by_id(update0))
 
 --- @type fun(bufnr: integer)
-local update_debounced
+M.update = nil
 
 function M.setup()
-  local group = api.nvim_create_augroup('gitsigns_blame', {})
-
-  local opts = config.current_line_blame_opts
-  update_debounced = debounce.debounce_trailing(opts.delay, update)
-
-  for k, _ in pairs(cache) do
+  for k in pairs(cache) do
     reset(k)
   end
 
-  if config.current_line_blame then
-    local events = { 'FocusGained', 'BufEnter', 'CursorMoved', 'CursorMovedI' }
-    if vim.fn.exists('#WinResized') == 1 then
-      -- For nvim 0.9+
-      events[#events + 1] = 'WinResized'
-    end
-
-    api.nvim_create_autocmd(events, {
-      group = group,
-      callback = function(args)
-        reset(args.buf)
-        update_debounced(args.buf)
-      end,
-    })
-
-    api.nvim_create_autocmd({ 'InsertEnter', 'FocusLost', 'BufLeave' }, {
-      group = group,
-      callback = function(args)
-        reset(args.buf)
-      end,
-    })
-
-    api.nvim_create_autocmd('OptionSet', {
-      group = group,
-      pattern = { 'fileformat', 'bomb', 'eol' },
-      callback = function(args)
-        reset(args.buf)
-      end,
-    })
-
-    update_debounced(api.nvim_get_current_buf())
+  if not config.current_line_blame then
+    return
   end
+
+  local group = api.nvim_create_augroup('gitsigns_blame', {})
+  local opts = config.current_line_blame_opts
+  M.update = debounce.debounce_trailing(opts.delay, update)
+
+  local events = { 'FocusGained', 'BufEnter', 'CursorMoved', 'CursorMovedI' }
+  if vim.fn.exists('#WinResized') == 1 then
+    -- For nvim 0.9+
+    events[#events + 1] = 'WinResized'
+  end
+
+  api.nvim_create_autocmd(events, {
+    group = group,
+    callback = function(args)
+      reset(args.buf)
+      M.update(args.buf)
+    end,
+  })
+
+  api.nvim_create_autocmd({ 'InsertEnter', 'FocusLost', 'BufLeave' }, {
+    group = group,
+    callback = function(args)
+      reset(args.buf)
+    end,
+  })
+
+  api.nvim_create_autocmd('OptionSet', {
+    group = group,
+    pattern = { 'fileformat', 'bomb', 'eol' },
+    callback = function(args)
+      reset(args.buf)
+    end,
+  })
 end
 
 return M

lua/gitsigns/attach.lua
@@ -357,6 +357,10 @@ local attach_throttled = throttle_by_id(function(cbuf, ctx, aucmd)
 
   -- Initial update
   manager.update(cbuf)
+
+  if config.current_line_blame then
+    require('gitsigns.current_line_blame').update(cbuf)
+  end
 end)
 
 --- Detach Gitsigns from all buffers it is attached to.

commit ae6fed967ffb15d48052cbff84b410ea16bdb7f0

fix(blame): show current buffer line blame immediately
fix a problem caused by commit: https://github.com/lewis6991/gitsigns.nvim/commit/8df63f2ddc615feb71fd4aee45a4cee022876df1

lua/gitsigns/current_line_blame.lua
@@ -223,6 +223,9 @@ function M.setup()
   local opts = config.current_line_blame_opts
   M.update = debounce.debounce_trailing(opts.delay, update)
 
+  -- show current buffer line blame immediately
+  M.update(api.nvim_get_current_buf())
+
   local events = { 'FocusGained', 'BufEnter', 'CursorMoved', 'CursorMovedI' }
   if vim.fn.exists('#WinResized') == 1 then
     -- For nvim 0.9+